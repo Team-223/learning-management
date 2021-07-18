23f7e97554b6aedcee86ee82b206297f1414782c
Orkid <mrs.emadi@yahoo.com>
2021-07-19 03:10:39 +0430

    ADD:alert-component

---
+++ src/components/announcement/Announcement.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Announcement.module.css';
+
+function Announcement() {
+    return (
+        <div className={styles.announcement__container}>
+            <span>no announcement currently!</span>
+        </div>
+    )
+}
+
+export default Announcement
