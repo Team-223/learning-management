ea50809c5b9636271eb6a3d104b37feeb2f7b35d
ehodg <ehodge@my.waketech.edu>
2021-07-19 16:55:40 -0400

    pull request

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import LandingPage from './pages/LandingPage'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Filter />
-=======
-      <LandingPage />
->>>>>>> 3c4db01aab52a793b4eb086fa435e32eefd4371c
     </div>
   );
 }
