418834723a4030f563629526b26d8127880874e8
Orkid <mrs.emadi@yahoo.com>
2021-07-21 23:03:20 +0430

    UPD:student-module

--- src/components/studentModules/StudentModules.js
+++ src/components/studentModules/StudentModules.js
@@ -8,31 +8,25 @@ function StudentModules() {
     const [expanded, setExpanded] = useState(false);
     return (
         <div className={styles.modules__container}>
-            <div className={styles.modules__row} >
+            <div className={styles.modules__row}>
                 <div className={styles.modules__left}>
                     <img  className={styles.icon} src='/images/modules-logo.png' alt='modules-logo'/>
                     <span className={styles.modules__titile}>Module 1 Tilte</span>
                 </div>
                 <div  className={styles.modules__right} onClick={()=> {setExpanded(expanded => !expanded) }}>
-                    <img src={ expanded ? arrowUp : arrowDown}  alt='arrows'/>
-                    {expanded && <StudentExercise />}
-                   
+                    <img className={styles.module__arrow} src={ expanded ? arrowUp : arrowDown}  alt='arrows'/>
+                    
                 </div>
             </div>
-
-            <div className={styles.modules__row} >
-                <div className={styles.modules__left}>
-                    <img  className={styles.icon} src='/images/modules-logo.png' alt='modules-logo'/>
-                    <span className={styles.modules__titile}>Module 1 Tilte</span>
+            {expanded && 
+                <div className={styles.exercise__row}>
+                    <img className={styles.exercise__icon} src='/images/exercises-logo.png' alt='ex-logo' />
+                    <span className={styles.exercise__title}>Exercise 1 Tilte</span>
                 </div>
-                <div  className={styles.modules__right} onClick={()=> {setExpanded(expanded => !expanded) }}>
-                    <img src={ expanded ? arrowUp : arrowDown}  alt='arrows'/>
-                    {expanded && <StudentExercise />}
-                   
-                </div>
-            </div>
-
-        </div>
+            }
+            
+          
+         </div>
     )
 }
 
