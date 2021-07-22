c903f293d8ae5dd65480bf90058bad243377f9a9
Team-223 <87396224+Team-223@users.noreply.github.com>
2021-07-22 17:14:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Filter from './components/Filter';
+import Header from './components/header/Header'
 import './App.css';
+import LandingPage from './pages/LandingPage'
 
 function App() {
   return (
     <div className="App">
-      <Filter />
+      <Header />
+      <LandingPage />
     </div>
   );
 }
