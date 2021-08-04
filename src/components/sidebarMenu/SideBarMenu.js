3a6c5faf351e10c1eb7b1c4d8ade3b16301ac5b1
Carlos <solrak29@yahoo.com>
2021-08-03 22:50:25 -0400

    cleaning up css and adding begining filter box

--- src/components/sidebarMenu/SideBarMenu.js
+++ src/components/sidebarMenu/SideBarMenu.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import styles from './SideBarMenu.module.css'
 
-export default function SideBarMenu() {
+export default function SideBarMenu({children}) {
+    const [xPos, setX] = useState(0)
+    const [width, setWidth] = useState(-300)
+
+    useEffect(() => {
+        //setWidth(300)
+        //setX(-50)
+    })
+
     return (
-        <div className={styles.sidebar_menu_container}>
+        <div className={styles.sidebar_menu_container}
+        style={{ width: width, transform: `translatex(${xPos}px)`}}>
             <div className={styles.sidebar_menu_content}>
-                <p>Modules</p>
-                <p>Exercises</p>
-                <p>Notes</p>
-                <p>Announcements</p>
+                {children}
+            </div>
+            <div className={styles.sidebar_filter_box}>
+                <p>Filter Results</p>
+                <input type="text" id="test" name="test"/>
+                <p>level list</p>
+                <input type="text" id="test" name="test"/>
             </div>
         </div>
     )
